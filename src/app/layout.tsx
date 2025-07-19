d878fba554e113f23682914834fcdfa38e18dfe4
Boris Cvorovic <boris@endavu.com>
2025-07-19 20:32:31 +0200

    Install: Tanstack query

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from "next"
 import { Geist, Geist_Mono } from "next/font/google"
+import { TanstackQueryProvider } from "@/providers/tanstack-query-provider/tanstack-query-provider"
 import "./globals.css"
 
 const geistSans = Geist({
@@ -27,7 +28,7 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
-        {children}
+        <TanstackQueryProvider>{children}</TanstackQueryProvider>
       </body>
     </html>
   )
